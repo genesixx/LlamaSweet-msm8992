ec24197bbc82ef7282d05a4ddc8a8772a0c2f910
jollaman999 <admin@jollaman999.com>
2016-06-03 23:20:54 +0900

    msm_hotplug: Do not handle fb notifier when msm_hotplug disabled

--- drivers/soc/qcom/msm_hotplug.c
+++ drivers/soc/qcom/msm_hotplug.c
@@ -1219,6 +1219,9 @@ static int msm_hotplug_fb_notifier_callback(struct notifier_block *self,
 	struct fb_event *evdata = data;
 	int *blank;
 
+	if (!msm_enabled)
+		return 0;
+
 	if (event == FB_EVENT_BLANK) {
 		blank = evdata->data;
 
