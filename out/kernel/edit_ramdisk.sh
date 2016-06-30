417ef42298d1da41e89f8da4861cf77fd381b01f
genesixx <erwanleboucher@gmail.com>
2016-06-30 21:11:34 +0200

    remove that

--- out/kernel/edit_ramdisk.sh
+++ out/kernel/edit_ramdisk.sh
@@ -13,17 +13,6 @@ cd /tmp/ramdisk/
 gunzip -c /tmp/ramdisk/boot.img-ramdisk.gz | cpio -i
 cd /
 
-echo "" >> /tmp/ramdisk/init.qcom.rc
-echo "service llama-post-boot /system/bin/sh /init.llama.post_boot.sh" >> /tmp/ramdisk/init.qcom.rc
-echo "class late_start" >> /tmp/ramdisk/init.qcom.rc
-echo "user root" >> /tmp/ramdisk/init.qcom.rc
-echo "disabled" >> /tmp/ramdisk/init.qcom.rc
-echo "" >> /tmp/ramdisk/init.qcom.rc
-echo "on property:sys.boot_completed=1" >> /tmp/ramdisk/init.qcom.rc
-echo "start llama-post-boot" >> /tmp/ramdisk/init.qcom.rc
-
-
-cp /tmp/init.llama.post_boot.sh /tmp/ramdisk/init.llama.post_boot.sh
 
 rm /tmp/ramdisk/boot.img-ramdisk.gz
 rm /tmp/boot.img-ramdisk.gz
